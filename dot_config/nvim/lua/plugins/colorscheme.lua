e0bfbfac7c14b6961ed003b51165b21c30e97246
Miguel Maramara <miguel.maramara@stonybrook.edu>
2024-03-18 17:58:16 -0400

    color schemes and buffer line!

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "loctvl842/monokai-pro.nvim" },
+  { "folke/tokyonight.nvim" },
+  -- Configure lazyvim to load proper theme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
+
+      -- NOTE: Some of my favorites:
+      -- colorscheme = "monokai-pro-classic",
+      -- colorscheme = "monokai-pro-ristretto",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "catppuccin-mocha",
+      -- colorscheme = "tokyonight-night",
+      --
     },
   },
 }
