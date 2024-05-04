c1a4c17a59af9916a0fd801994428bd747755c0a
miguelcmaramara <miguelcmaramara@gmail.com>
2024-05-04 00:34:59 -0400

    Update .config/alacritty/alacritty.toml Update .config/hypr/configs/monitors.conf Update .config/hypr/configs/settings.conf Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/tmux/tmux.conf Update .config/zsh/.zshrc Update .local/bin/tmux-sessionizer Update .zprofile

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.util.dot" },
